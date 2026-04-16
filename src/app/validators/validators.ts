1d8edd5feae05779ed73b5e0746e714d47c6c7cb
manoj.jaiswal <mjaiswal@landolakes.com>
2026-04-16 14:17:17 +0530

    added form array for phone number

--- src/app/validators/validators.ts
+++ src/app/validators/validators.ts
@@ -23,3 +23,19 @@ export function minUppercase(count: number) : (control: AbstractControl) => {[ke
         return null;
     };
 }
+
+export function passwordsMatch(control: AbstractControl): {[key: string]: boolean} | null
+{
+    const password = control.get('password');
+    const confirmPassword = control.get('confirmPassword');
+
+    if(!password || !confirmPassword)
+    {
+        return null;
+    }
+    if(password.value !== confirmPassword.value)
+    {
+        return { 'passwordMismatch': true };
+    }
+    return null;
+}
